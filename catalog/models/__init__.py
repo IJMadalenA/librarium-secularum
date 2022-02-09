53fc61d375097e7e3e743533eaa7884cd0b71bd5
IsMadalena <IsMadalena@user.github.com>
2022-02-09 21:29:31 +0100

    Creating book_instance, publisher and relationship models. Server Ok.

--- catalog/models/__init__.py
+++ catalog/models/__init__.py
@@ -2,3 +2,5 @@ from .author import Author
 from .book import Book
 from .genre import Genre
 from .relationship_book_author import RelationshipBookAuthor
+from .publisher import Publisher
+from .book_instance import BookInstance
---
+++ catalog/models/publisher.py
@@ -0,0 +1,14 @@
+# Imported from Django.
+from django.db import models
+
+class Publisher(models.Model):
+	name = models.CharField(
+		max_length=128,
+		null=False,
+		blank=False,
+	)
+	country = models.CharField(
+		max_length=128,
+		null=False,
+		blank=False,
+	)
