d7bda4def475957a2f715b0ff6109526a50694a7
IsMadalena <IsMadalena@user.github.com>
2022-02-13 16:01:13 +0100

    First test. Author model test. Testing Ok.

--- catalog/tests/models_tests/author_model_tests.py
+++ catalog/tests/models_tests/author_model_tests.py
@@ -13,5 +13,4 @@ class AuthorModelTestCase(TestCase):
 	def test_author_model(self):
 		before_create = Author.objects.count()
 		author = AuthorFactory()
-		
 		self.assertEqual(Author.objects.count(), before_create + 1)
