698c252a7dce9052a076acc9a60e5916abe3480a
IsMadalena <IsMadalena@user.github.com>
2022-02-14 22:07:39 +0100

    Creating book factory. Testing Ok.

--- catalog/factories/book_factory.py
+++ catalog/factories/book_factory.py
@@ -1,1 +1,18 @@
-d
+# Imported from Factory-Boy.
+from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyText
+from factory.fuzzy import FuzzyDate
+
+# Models.
+from catalog.models import Book
+
+# Utils.
+import datetime
+
+
+class BookFactory(DjangoModelFactory):
+	
+	class Meta:
+		model = Book
+		
+		
