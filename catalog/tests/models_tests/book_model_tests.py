2a32b95336873c5e038318ec39a22956032bd8b7
IJMadalena <IJMadalena@outlook.com>
2022-03-02 23:30:29 +0100

    Create book_model_tests. Testing Ok.

---
+++ catalog/tests/models_tests/book_model_tests.py
@@ -0,0 +1,17 @@
+# Imported from Django.
+from django.test import TestCase
+
+# Factories.
+from catalog.factories import BookFactory
+
+# Models.
+from catalog.models import Book
+
+
+class BookModelTestCase(TestCase):
+
+    def test_book_model(self):
+        before_create = Book.objects.count()
+        book = BookFactory()
+        self.assertTrue(Book.objects.filter(id=book.id).exists())
+        self.assertEqual(Book.objects.count(), before_create +1)
